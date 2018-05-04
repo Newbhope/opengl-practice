7728817d26f2066521d336d5c8bd93871980f5c3
jpham8 <newhope11134@gmail.com>
2018-05-04 00:47:04 -0700

    Can't get 4 dots on the screen

--- Headers/glitter.hpp
+++ Headers/glitter.hpp
@@ -12,12 +12,16 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <iostream>
+#include <string>
+
 // Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
 // To use stb_image, add this in *one* C++ source file.
 //     #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-// Define Some Constants
+const std::string MODE = "geometry";
+
 const int mWidth = 1280;
 const int mHeight = 720;
 
@@ -54,5 +58,72 @@ void main() {
 }
 
 )glsl";
+
+const char* geoVertex = R"glsl(
+#version 150 core
+
+in vec2 pos;
+
+void main() {
+	gl_Position = vec4(pos, 0.0, 1.0);
+}
+
+)glsl";
+
+const char* geoFrag = R"glsl(
+#version 150 core
+
+out vec4 outColor;
+
+void main() {
+	outColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+
+)glsl";
+
+const char* geoShader = R"glsl(
+#version 150 core
+
+layout(points) in;
+layout(line_strip, max_vertices = 1) out;
+
+void main() {
+	gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.0, 0.0, 0.0);
+	EmitVertex();
+
+	gl_Position = gl_in[0].gl_Position + vec4(0.1, 0.0, 0.0, 0.0);
+	EmitVertex();
+
+	EndPrimitive();
+}
+
+)glsl";
+
+// Vertex shader
+const GLchar* vertexShaderSrc = R"glsl(
+    #version 150 core
+
+    in vec2 pos;
+
+    void main()
+    {
+        gl_Position = vec4(pos, 0.0, 1.0);
+    }
+
+)glsl";
+
+// Fragment shader
+const GLchar* fragmentShaderSrc = R"glsl(
+    #version 150 core
+
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+)glsl";
+
+//methods
+GLuint createShader(GLenum, const GLchar*);
 
 #endif //~ Glitter Header
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -32,122 +32,189 @@ int main() {
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
-    // Create Vertex Array Object
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+	if (MODE == "geometry") {
+		GLuint vertexShader = createShader(GL_VERTEX_SHADER, geoVertex);
+		GLuint fragmentShader = createShader(GL_FRAGMENT_SHADER, geoFrag);
+		GLuint geometryShader = createShader(GL_GEOMETRY_SHADER, geoShader);
 
-    // Create a Vertex Buffer Object and copy the vertex data to it
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-	/*
-	float vertices[] = {
-		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
-		0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+		GLuint shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glAttachShader(shaderProgram, geometryShader);
+		glLinkProgram(shaderProgram);
+		glUseProgram(shaderProgram);
 
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
-		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f, // Bottom-left
-		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f  // Top-left
-	};
-	*/
+		GLuint vbo;
+		glGenBuffers(1, &vbo);
 
-	float vertices[] = {
-		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
-		0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
-		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
-	};
+		float points[] = {
+			-0.45f,  0.45f,
+			0.45f,  0.45f,
+			0.45f, -0.45f,
+			-0.45f, -0.45f,
+		};
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
-	//Element buffer object (uses vertex buffer somehow?)
+		// Create VAO
+		GLuint vao;
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
 
-	GLuint elements[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-	GLuint ebo;
-	glGenBuffers(1, &ebo);
+		// Specify layout of point data
+		GLint posAttrib = glGetAttribLocation(shaderProgram, "pos");
+		glEnableVertexAttribArray(posAttrib);
+		glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-		sizeof(elements), elements, GL_STATIC_DRAW);
 
-	//Somehow read an external shader file I guess
-	//Or just deal with ugly shader string constants
+		while (glfwWindowShouldClose(mWindow) == false) {
+			if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+				glfwSetWindowShouldClose(mWindow, true);
+			}
 
-    // Create and compile the vertex shader
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
-    glCompileShader(vertexShader);
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-    // Create and compile the fragment shader
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-    glCompileShader(fragmentShader);
+			//glDrawArrays(GL_POINTS, 0, 4);
 
-    // Link the vertex and fragment shader into a shader program
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glBindFragDataLocation(shaderProgram, 0, "outColor");
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
 
-    // Specify the layout of the vertex data
-	/*
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	*/
-
-	GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
-
-	auto t_start = std::chrono::high_resolution_clock::now();
-
-	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
-		5 * sizeof(float), 0);
-
-	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
-	glEnableVertexAttribArray(colAttrib);
-	glVertexAttribPointer(colAttrib,
-		3,
-		GL_FLOAT,
-		GL_FALSE,
-		5 * sizeof(float),
-		(void*)(2 * sizeof(float)));
-
-    // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-		if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-			glfwSetWindowShouldClose(mWindow, true);
+			// Flip Buffers and Draw
+			glfwSwapBuffers(mWindow);
+			glfwPollEvents();
 		}
 
-        // Background Fill Color
-        glClearColor(0.f, 0.f, 0.f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+		glfwTerminate();
+		return EXIT_SUCCESS;
+	}
 
-		//For drawing vertex point arrays
-		//glDrawArrays(GL_TRIANGLES, 0, 6);
+	else {
 
-		//For drawing element buffers
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// Create Vertex Array Object
+		GLuint vao;
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
 
-        // Flip Buffers and Draw
-        glfwSwapBuffers(mWindow);
-        glfwPollEvents();
+		// Create a Vertex Buffer Object and copy the vertex data to it
+		GLuint vbo;
+		glGenBuffers(1, &vbo);
+		/*
+		float vertices[] = {
+			-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+			0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+			0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
 
-		auto t_now = std::chrono::high_resolution_clock::now();
-		float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+			0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+			-0.5f, -0.5f, 1.0f, 1.0f, 1.0f, // Bottom-left
+			-0.5f,  0.5f, 1.0f, 0.0f, 0.0f  // Top-left
+		};
+		*/
 
-		glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
-    }   
-	
-	glfwTerminate();
+		float vertices[] = {
+			-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+			0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+			0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+			-0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+		};
 
-    return EXIT_SUCCESS;
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		//Element buffer object (uses vertex buffer somehow?)
+
+		GLuint elements[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
+		GLuint ebo;
+		glGenBuffers(1, &ebo);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+			sizeof(elements), elements, GL_STATIC_DRAW);
+
+		//Somehow read an external shader file I guess
+		//Or just deal with ugly shader string constants
+
+		// Create and compile the vertex shader
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vertexSource, NULL);
+		glCompileShader(vertexShader);
+
+		// Create and compile the fragment shader
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+		glCompileShader(fragmentShader);
+
+		// Link the vertex and fragment shader into a shader program
+		GLuint shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glBindFragDataLocation(shaderProgram, 0, "outColor");
+		glLinkProgram(shaderProgram);
+		glUseProgram(shaderProgram);
+
+		// Specify the layout of the vertex data
+		/*
+		GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+		glEnableVertexAttribArray(posAttrib);
+		glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+		*/
+
+		GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+
+		auto t_start = std::chrono::high_resolution_clock::now();
+
+		GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+		glEnableVertexAttribArray(posAttrib);
+		glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
+			5 * sizeof(float), 0);
+
+		GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+		glEnableVertexAttribArray(colAttrib);
+		glVertexAttribPointer(colAttrib,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			5 * sizeof(float),
+			(void*)(2 * sizeof(float)));
+
+		// Rendering Loop
+		while (glfwWindowShouldClose(mWindow) == false) {
+			if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+				glfwSetWindowShouldClose(mWindow, true);
+			}
+
+			// Background Fill Color
+			glClearColor(0.f, 0.f, 0.f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			//For drawing vertex point arrays
+			//glDrawArrays(GL_TRIANGLES, 0, 6);
+
+			//For drawing element buffers
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+			// Flip Buffers and Draw
+			glfwSwapBuffers(mWindow);
+			glfwPollEvents();
+
+			auto t_now = std::chrono::high_resolution_clock::now();
+			float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+
+			glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
+		}
+
+		glfwTerminate();
+
+		return EXIT_SUCCESS;
+	}
+}
+
+GLuint createShader(GLenum type, const GLchar* src) {
+	GLuint shader = glCreateShader(type);
+	glShaderSource(shader, 1, &src, nullptr);
+	glCompileShader(shader);
+	return shader;
 }
