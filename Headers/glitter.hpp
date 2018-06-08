555a7b8ea58bb5617bcb7e3f90c0acf323c56f88
jpham8 <newhope11134@gmail.com>
2018-06-08 02:05:22 -0700

    I made some sort of abomination

--- Headers/glitter.hpp
+++ Headers/glitter.hpp
@@ -153,34 +153,63 @@ in float vBranches[];
 
 out vec3 fColor; //Output to fragment shader
 
+struct stack {
+    //initializes to 0 for some reason
+    int top;
+    vec4 positions[1000];
+};
 
-void createSubtree(in vec4 currentPosition, in int numLevelsLeft) {
-	gl_Position = currentPosition;
-	EmitVertex();
+stack treeStack;
 
-	//left branch
-	vec4 leftOffset = vec4(-0.1, 0.1, 0.0, 0.0);
-	gl_Position = currentPosition + leftOffset;
-	EmitVertex();
-
-	//move back to base of subtree
-	gl_position = currentPosition;
-	EmitVertex();
-
-	//right branch
-	vec4 rightOffset = vec4(0.1, 0.1, 0.0, 0.0);
-	gl_Position = currentPosition + rightOffset;
-	EmitVertex();
-
-	if (numLevelsLeft > 0) {
-		createSubtree(currentPosition + leftOffset, numLevelsLeft - 1);
-		createSubtree(currentPosition + rightOffset, numLevelsLeft + 1);
-	}
+void push(vec4 input) {
+    treeStack.positions[treeStack.top] = input;
+    treeStack.top++;
 }
 
+vec4 pop() {
+    /*
+    if (treeStack.top <= -1) {
+        return vec4(0., 0., 0., 0.);
+    }
+    */
+    vec4 result = treeStack.positions[treeStack.top];
+    treeStack.top--;
+    return result;
+}
 
 void main() {
     fColor = vColor[0];
+
+    treeStack.positions[1] = gl_in[0].gl_Position;
+    treeStack.top = 1;
+    for (int i = 0; i < 100; i++) {
+
+    //while (treeStack.top > -1) {
+
+        vec4 currentPosition = pop();
+        gl_Position = currentPosition;
+        EmitVertex();
+
+        //left branch
+        vec4 leftOffset = vec4(-0.1, 0.1, 0.0, 0.0);
+        vec4 leftPoint = currentPosition + leftOffset;
+        gl_Position = leftPoint;
+        EmitVertex();
+        push(leftPoint);
+
+        //move gl_Position back to base of branch
+    	gl_Position = currentPosition;
+    	EmitVertex();
+
+        //right branch
+        vec4 rightOffset = vec4(0.1, 0.1, 0.0, 0.0);
+        vec4 rightPoint = currentPosition + rightOffset;
+        gl_Position = rightPoint;
+        EmitVertex();
+        push(rightPoint);
+    }
+
+/*
 	gl_Position = gl_in[0].gl_Position;
 	EmitVertex();
 
@@ -198,10 +227,12 @@ void main() {
     gl_Position = gl_in[0].gl_Position + rightOffset;
     EmitVertex();
 
-	createSubtree(gl_in[0].gl_Position, vLevels[0]);
+	//createSubtree(gl_in[0].gl_Position, vLevels[0]);
+*/
 
     EndPrimitive();
 }
+
 
 )glsl";
 
